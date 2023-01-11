5adf06b7f46f1e71149cd84f27c77355ccd11681
Patrick Bryan <pbryan9@gmail.com>
2023-01-11 14:59:46 -0800

    complete thru sort

--- bash.js
+++ bash.js
@@ -5,6 +5,9 @@ const cat = require("./cat");
 const date = require("./date");
 const echo = require("./echo");
 const head = require("./head");
+const tail = require("./tail");
+const sort = require("./sort");
+const wc = require("./wc");
 //Output a prompt
 process.stdout.write("prompt > ");
 
@@ -15,6 +18,9 @@ process.stdin.on("data", (data) => {
   if (cmd === "ls") ls(done);
   if (cmd.split(" ")[0] === "cat") cat(cmd, done);
   if (cmd.split(" ")[0] === "head") head(cmd, done);
+  if (cmd.split(" ")[0] === "tail") tail(cmd, done);
+  if (cmd.split(" ")[0] === "sort") sort(cmd, done);
+  if (cmd.split(" ")[0] === "wc") wc(cmd, done);
   if (cmd === "date") date(done);
   if (cmd.split(" ")[0] === "echo") echo(cmd, done);
 });
